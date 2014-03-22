c9e5ecbf5731f41a7e21428e1f4362dcb4cce308
nevor1530 <nevor1530@163.com>
2014-03-22 15:43:34 +0800

    first section

--- js/index.js
+++ js/index.js
@@ -1,38 +1,55 @@
 $(function(){
 	// 响应式尺寸参数
 	var screen_xs_max = 768;
-	$window = $(window);
+
+	// 常用DOM对象
+	var $window = $(window);
 	
 	// 限制幻灯片的高度
-	$(".js-slide").height($window.height() - 10);
-	$window.resize(function(){
-		$(".js-slide").height($window.height() - 10);
+	var imgw = 1440, imgh = 900;
+	var $js_slides = $(".js-slide");
+	var $slide_img = $(".slide-img");
+	$(window).resize(function(){
+		onresize();
 	});
+	function onresize(){
+		var wh = $window.height()-10;
+		var ww = $window.width();
+		$js_slides.height(wh);
+		if (ww/wh >= imgw/imgh){
+			$slide_img.css("width","100%");
+			$slide_img.css("height","auto");
+			$slide_img.css("left", 0);
+		} else {
+			$slide_img.css("width","auto");
+			$slide_img.css("height","100%");
+			$slide_img.css("left", (-imgw*wh/imgh/2 + ww/2)+"px");
+		}
+	}
+	onresize();
 	
 	// 根据滚动方向，隐藏和显示header
+	var $header = $("#id-header");
 	var lastTop = 0, curTop = 0;
 	var headerStatus = 1;
-	$window.scroll(function(e){
-		if ($window.width() >= screen_xs_max){
-			curTop = $(this).scrollTop();
-			var $header = $("#id-header");
-			if (curTop < lastTop && !headerStatus){
-				// 向上
-				var params = {};
-				params.top = "0";
-				$header.animate(params);
-				headerStatus = 1;
-			} else if(curTop > lastTop && headerStatus && curTop > $header.outerHeight()){
-				// 向下
-				var params = {
-					top: -$header.outerHeight()+"px"
-				};
-				$header.animate(params);
-				headerStatus = 0;
-			}
-			setTimeout(function(){
-				lastTop = curTop;
-			}, 0);
+	$(window).scroll(function(e){
+		curTop = $(this).scrollTop();
+		if (curTop < lastTop && !headerStatus){
+			// 向上
+			var params = {};
+			params.top = "0";
+			$header.animate(params);
+			headerStatus = 1;
+		} else if(curTop > lastTop && headerStatus && curTop > $header.outerHeight()){
+			// 向下
+			var params = {
+				top: -$header.outerHeight()+"px"
+			};
+			$header.animate(params);
+			headerStatus = 0;
 		}
+		setTimeout(function(){
+			lastTop = curTop;
+		}, 0);
 	});
 });
