3f071da99ad5d3789d0f6e199fd25ec6f464f44d
nevor1530 <nevor1530@163.com>
2014-04-12 12:16:33 +0800

    adjust the partner section css

--- protected/views/site/_partner_section.php
+++ protected/views/site/_partner_section.php
@@ -1,45 +1,57 @@
 <section class="section partner-section" id="id-partner-section">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 col-sm-8 col-md-6 col-lg-6">
+			<div class="col-xs-12 col-sm-8 col-md-6">
 				<div class="overview animate" data-animate="fadeInDown">
 					<h3 class="section-title"><strong>对我们感兴趣？</strong></h3>
 					<p class="lead">留下您的联系方式，我们将尽快联系您!</p>
 				</div>
-				<form role="form" class="partner-form col-xs-12 col-sm-10 col-md-10 col-lg-10">
-					<p class="title text-center">公司/设计师信息</p>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 input-group">
-						<p class="lead">公司全称：</p>
-		            	<input type="text" class="form-control input-sm">
-		          	</div>
-		          	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 input-group">
-						<p class="lead">品牌：</p>
-		            	<input type="text" class="form-control input-sm">
-		          	</div>
-		          	<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 input-group">
-						<p class="lead">省份：</p>
-		            	<input type="text" class="form-control input-sm">
-		          	</div>
-		          	<div class="col-xs-offset-2 col-xs-5 col-sm-offset-2 col-sm-5 col-md-offset-2 col-md-5 col-lg-offset-2 col-lg-5 input-group city-container">
-						<p class="lead">城市：</p>
-		            	<input type="text" class="form-control input-sm">
-		          	</div>
-		          	<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 input-group">
-						<p class="lead">联系人：</p>
-		            	<input type="text" class="form-control input-sm">
-		          	</div>
-		          	<div class="col-xs-offset-2 col-xs-5 col-sm-offset-2 col-sm-5 col-md-offset-2 col-md-5 col-lg-offset-2 col-lg-5 input-group phone-container">
-						<p class="lead">联系电话：</p>
-		            	<input type="text" class="form-control input-sm">
-		          	</div>
-		          	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 input-group">
-						<p class="lead">电子邮箱：</p>
-		            	<input type="text" class="form-control input-sm">
-		          	</div>
-	          		<span class="text-center input-group-btn">
-	              		<button class="confirm-btn" type="button">确定</button>
-	          		</span>
-	          	</form>
+				<form role="form" class="partner-form" style="width:100%">
+					<div class="row"><p class="title text-center col-xs-12">公司/设计师信息</p></div>
+					<div class="row">
+						<div class="col-xs-12">
+							<p class="lead">公司全称：</p>
+			  	          		<input type="text" class="form-control input-sm">
+			  	        	</div>
+					</div>
+					<div class="row">
+			  	        	<div class="col-xs-12">
+			  	      			<p class="lead">品牌：</p>
+			  	          		<input type="text" class="form-control input-sm">
+			  	        	</div>
+					</div>
+					<div class="row">
+			  	        	<div class="col-xs-6">
+			  	      			<p class="lead">省份：</p>
+			  	          		<input type="text" class="form-control input-sm">
+			  	        	</div>
+		  	        		<div class="col-xs-6 city-container">
+		  	      				<p class="lead">城市：</p>
+		  	        	  		<input type="text" class="form-control input-sm">
+		  	        		</div>
+					</div>
+					<div class="row">
+			  	        	<div class="col-xs-6">
+			  	      			<p class="lead">联系人：</p>
+			  	          		<input type="text" class="form-control input-sm">
+			  	        	</div>
+			  	        	<div class="col-xs-6 phone-container">
+			  	      			<p class="lead">联系电话：</p>
+			  	          		<input type="text" class="form-control input-sm">
+			  	        	</div>
+					</div>
+					<div class="row">
+			  	        	<div class="col-xs-12">
+			  	      			<p class="lead">电子邮箱：</p>
+			  	          		<input type="text" class="form-control input-sm">
+			  	        	</div>
+					</div>
+					<div class="row">
+		          			<center class="col-xs-12">
+		          	    			<button class="confirm-btn" type="button">确定</button>
+		          			</center>
+					</div>
+		          	</form>
 			</div>
 		</div>
 	</div>
