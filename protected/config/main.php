b37da6aea369b80dc1f42885af837e107b521d39
nevor1530 <nevor1530@163.com>
2014-04-22 18:02:37 +0800

    add timezone set

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,6 +9,7 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'IsOften',
 	'language'=>'zh_cn',
+	'timeZone'=>'Asia/Shanghai',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
